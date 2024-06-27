f7a0bb3f79d2009fc8981a42e58daff2cf6a477c
daniehei <daniel.heimgartner@ivt.baug.ethz.ch>
2024-06-27 16:18:02 +0200

    corrected my sample_sizes

--- data-raw/response_rates.R
+++ data-raw/response_rates.R
@@ -16,15 +16,6 @@ rr <- readxl::read_xlsx(path, n_max = nmax)
 
 names(rr)
 
-rr <-
-  rr %>%
-  mutate(flag_yes_no = yes_recruitment_no_incentive != 0,
-         flag_yes_yes = yes_recruitment_yes_incentive != 0,
-         flag_no_no = no_recruitment_no_incentive != 0) %>%
-  select(-c(yes_recruitment_no_incentive,
-            yes_recruitment_yes_incentive,
-            no_recruitment_no_incentive))
-
 response_rates <- rr
 
 usethis::use_data(response_rates, overwrite = TRUE)
